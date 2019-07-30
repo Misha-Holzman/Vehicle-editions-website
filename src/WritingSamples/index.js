bd58a5d91862375f46bb82e1ca4e2a8612d8887c
Misha Holzman <mcholzman1@gmail.com>
2019-07-30 13:03:08 -0400

    wirting samples component complete. Donna Dennis passage added.

--- src/WritingSamples/index.js
+++ src/WritingSamples/index.js
@@ -11,15 +11,81 @@ class WritingSamples extends Component {
         </div>
         <div className='writing-samples-main-div'>
           <div className='donna-dennis-passage-div'>
-            <a href='#' className='link-to-dennis-passage' target='_blank'><h3 className='dennis-passage-title'>Passage: Donna Dennis at Lesley Heller</h3></a>
-            <p className='dennis-passage-author'>by Annabel Lee</p>
-            <p className='dennis-passage-name-of-show'>Donna Dennis: Ship and Dock/Nights and Days or The Gazer at Lesley Heller Gallery</p>
-            <a href='#' target='_blank' className='dennis-passage-gallery-link'>
-              <p className='dennis-passage-gallery'>Lesley Heller Gallery</p>
-            </a>
-            <p className='dennis-passage-date'>May 31 to June 30, 2018</p>
-            <p className='dennis-passage-address'>54 Orchard Street, New York City </p>
-            <p className='dennis-passage-article'>Read the article at<a href='http://www.artcritical.com/2018/06/18/annabel-lee-on-donna-dennis/' target='_blank' rel='noopener noreferrer' className='dennis-passage-gallery-link'> ArtCritical.com </a></p>
+            <h3 className='dennis-passage-title dennis-text'><a href='#' className='link-to-dennis-passage' target='_blank'>Passage: Donna Dennis at Lesley Heller</a></h3>
+            <p className='dennis-passage-author dennis-text'>by Annabel Lee</p>
+            <p className='dennis-passage-name-of-show dennis-text'>Donna Dennis: Ship and Dock/Nights and Days or The Gazer at Lesley Heller Gallery</p>
+            <p className='dennis-passage-gallery dennis-text'><a href='#' target='_blank' className='dennis-passage-gallery-link'>Lesley Heller Gallery</a></p>
+            
+            <p className='dennis-passage-date dennis-text'>May 31 to June 30, 2018</p>
+            <p className='dennis-passage-address dennis-text'>54 Orchard Street, New York City </p>
+            <p className='dennis-passage-article dennis-text'>Read the article at <a href='http://www.artcritical.com/2018/06/18/annabel-lee-on-donna-dennis/' target='_blank' rel='noopener noreferrer' className='dennis-passage-gallery-link'>ArtCritical.com</a></p>
+            <img src='https://i.imgur.com/ir9dzy3.jpg' className='dennis-img-main' alt='' />            
+            <p className='dennis-text dennis-article-writing'>
+              Have you ever wanted to walk inside a painting, sit down and experience the work from the inside? Ship and Dock/Nights
+              and Days or The Gazer is like a painting: you have to keep looking at what is before you so you know not only what you’re
+              seeing but what you’re feeling as well. This mixed media assemblage, the primary work in this exhibition, takes up an
+              entire room and carries psychological power.
+            </p>
+            <p className='dennis-text dennis-article-writing'>
+              Entering a darkened space through a floor-to-ceiling black curtain, you encounter a structure beyond which, in one direction,
+              is a slowly, gradually changing sky beginning at the horizon line. The whole installation is miniaturized yet human-scale,
+              like a great big, exploded-open Joseph Cornell box.
+            </p>
+            <p className='dennis-text dennis-article-writing'>
+              A bench is placed conveniently against a dark wall pierced with scattered holes that allow tiny and tinier lights to shine through.
+              Sit down with the starry wall behind you and gaze at, and through, the structure. It’s mesmerizing and invites you to linger and
+              contemplate the passage of time. The structure and the walls on either side and behind are dark with a greater depth than any empty
+              black because this color is, in fact, a dark marine blue.
+            </p>
+            <p className='dennis-text dennis-article-writing'>
+              The structure is a dock with two sheds. One weather beaten shed, nearer the viewer, to the left on the dock, is clad in corrugated
+              metal with authentic signs of aging: dings and scrapes. Its door has a weathered, cross-hatched safety-glass window through which
+              a lit bulb exudes orangey-yellowish light with a brighter center, like the glow from a gas lamp, only the filament identifies this
+              lamp as an industrial-type, if delicate electrical bulb. On entering the space with its many parts, darkness and this light, this
+              lamp suggests a Bec Auer gas lamp of the type familiar from Marcel Duchamp’s Etant Donnés. We look through something to see a view
+              beyond, a light on the other side of the solid structure. Only there is no figure here except oneself and a haunting feeling of
+              loneliness. Like Duchamp, Donna Dennis has controlled the conditions under which the viewer experiences the work. One must be part
+              of the work to see it.
+            </p>
+            <img src='https://i.imgur.com/qJlAAtH.jpg' className='dennis-img-secondary' alt='' />
+            <p className='dennis-text dennis-article-writing'>The piece changes, even though nothing moves but light. This is not a static sculpture. There is a subtle though constant state
+              of change, augmented by the gentle sounds of water lapping up on a shore, swirls and eddies, droplets of water, tidal sounds.
+              Waves pull back, rigging that is not tied fast quietly clatters, breezes funnel through the dock and over water. There’s an eerie
+              cosmic whoosh that complements the surrounding darkness.
+            </p>
+            <p className='dennis-text dennis-article-writing'>
+              The other shed, further from the viewer and to the far right at the end of a walkway, stands on an elevated structure with a dark
+              side facing the viewer and a light side facing left. This light side lends a surreal quality to the scene. The gravity and sharp
+              contrasts of light and dark on the sides of this form suggest the melancholy of Giorgio de Chirico’s abandoned cityscapes and
+              the relationships he creates between buildings in space.
+            </p>
+            <p className='dennis-text dennis-article-writing'>
+              The dock is set on what look like concrete pylons whose square footings suggest water underneath. The viewer may be walking
+              on that water. The crossbeams, the gangplank and stairways ck create many geometric shapes, like erector sets or metal shelving:
+              crisscrossing patterns and a variety of exes and rectangular framing. Drooping cables or fuel pipes connect into stanchions that
+              resemble plumbing fixtures, rounded at the top, as if they have shut-off valves or switches for the flow of liquids or current
+              running inside the sheathing. Or they are hawsers, protected from the elements inside metal tubing where those strong cables
+              penetrate the deck.
+            </p>
+            <img src='https://i.imgur.com/CB98CBS.jpg' className='dennis-img-secondary' alt='' />
+            <p className='dennis-text dennis-article-writing'>
+              This is a durational work because beyond the dock the projection of sky above the horizon changes gradually from day to night to day,
+              from painterly sky blues to dark night with brushstroked stars as a ship changes from white against the night sky to black against
+              the daytime skies in the distance. Thus the elements of engineering and technology that exist here in a three-dimensional space,
+              also includes the fourth dimension of time. And, though that horizon changes, it’s always night for the viewer with the stars
+              shining behind us..
+            </p>
+            <p className='dennis-text dennis-article-writing'>
+              This scene may represent a vast lake in northern Minnesota where typically fish houses and small cabin structures can be seen
+              from the shore. The scale of the work suggests that this may represent one of the Great Lakes because the ship, an ore ship
+              carrying coal, is so small, so far in the distance. And what looks like a heap of rubble under the dock suggests piled up coal
+            </p>
+            <p className='dennis-text dennis-article-writing'>
+              The viewer is struck by the precision Donna Dennis employed in creating the overall construction, the sound and the lighting.
+              This reflects the attention brought to bear on the locations where the artist made her beautiful gouache drawings that hang in
+              a separate room, and the elaborate preparations to accomplish her installation, first constructed in the studio, then disassembled
+              and recreated at Lesley Heller.
+            </p>
           </div>
           <div className='brown-harris-pages'>
             <img src='https://i.imgur.com/asKV8Xn.jpg' alt='' />
